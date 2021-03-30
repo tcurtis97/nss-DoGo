be8b09fc417cb373ac8248043582fb2f7cda4889
Tim Curtis <timcurtis97@yahoo.com>
2021-03-30 15:40:26 -0500

    added validation attributes for dog

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,25 @@ namespace DogGo.Models
     public class Dog
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Breed...")]
         public string Breed { get; set; }
-       
+
+
+        [Required]
         public int OwnerId { get; set; }
+        
+        
         public string ImageUrl { get; set; }
+        
+        
         public Owner Owner { get; set; }
+
+        [Required]
         public string Notes { get; set; }
     }
 }
